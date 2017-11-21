3bbd8a1551ee01b5eaf3849a268ed5e1649a23ea
Selahattin ÇİNÇİN <selocincin@hotmail.com>
2017-11-21 17:56:19 +0300

    Initial Commit

---
+++ JSONParseTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONParseTest
+//
+//  Created by Selahattin Cincin on 21.11.2017.
+//  Copyright © 2017 Selahattin Cincin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
