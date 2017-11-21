f10a01b371c212e892275f517fb03d9098d0abb0
Selahattin ÇİNÇİN <selocincin@hotmail.com>
2017-11-21 18:43:22 +0300

    nn

--- JSONParseTest/ViewController.swift
+++ JSONParseTest/ViewController.swift
@@ -6,41 +6,43 @@
 //  Copyright © 2017 Selahattin Cincin. All rights reserved.
 //
 
-
 import UIKit
 
+struct Values: Decodable {
+    let base: String
+    let date: String
+    let rates: [Rates]
+}
+struct Rates: Decodable {
+    let TRY:Float?
+}
+
 class ViewController: UIViewController {
-    
-    
-    override func viewDidLoad()
-    {
+
+    override func viewDidLoad() {
         super.viewDidLoad()
+         // Do any additional setup after loading the view, typically from a nib.
         
-        let url = URL(string:"https://api.fixer.io/latest")
-        let task = URLSession.shared.dataTask(with: url!) { (data,response,error) in
-            if error != nil{
-                print("ERROR")
-            }
-            else{
-                if let content = data{
-                    do{
-                        //array
-                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
-                        print(myJson)
-                        
-                    }
-                    catch{
-                        
-                    }
-                }
-            }
+        let jsonUrlString = "https://api.fixer.io/latest"
+        guard let url = URL(string: jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+        guard let data = data else {return}
+        do{
+                
+            let money = try JSONDecoder().decode(Values.self, from: data)
+            print(money.base)
             
-        }
-        task.resume()
-        
+            }catch let jsonErr{
+                print("error json:", jsonErr)
+                
+                }
+            }.resume()
+       
     }
-    
 
     
+
+
 }
 
