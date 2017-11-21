c549c8b9ff408278922f9ce56577f827f1a5e886
Selahattin ÇİNÇİN <selocincin@hotmail.com>
2017-11-21 18:34:51 +0300

    new

--- JSONParseTest/ViewController.swift
+++ JSONParseTest/ViewController.swift
@@ -6,20 +6,49 @@
 //  Copyright © 2017 Selahattin Cincin. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var paraBirimi: UITextField!
+    @IBOutlet weak var sonuc: UITextField!
+    @IBAction func deger(_ sender: Any) {
+        
     }
-
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        let url = URL(string:"https://api.fixer.io/latest")
+        let task = URLSession.shared.dataTask(with: url!) { (data,response,error) in
+            if error != nil{
+                print("ERROR")
+            }
+            else{
+                if let content = data{
+                    do{
+                        //array
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        print(myJson)
+                        
+                    }
+                    catch{
+                        
+                    }
+                }
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    
 }
 
